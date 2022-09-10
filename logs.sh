44dc9a1a83987c29aaf8899bcf1cc6bbf258d46a
Manan <immnan333@gmail.com>
2022-09-10 12:40:26 +0530

    version 1.0

--- logs.sh
+++ logs.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-echo
+echo 
 
-date >> crane.log
+date | tee -a crane.log
 
-sudo docker images >> crane.log
+sudo docker images | tee -a crane.log
 
-sudo docker ps -a >> crane.log
+sudo docker ps | tee -a crane.log
 
-sudo docker ps
-echo "Copy the container ID of blazemeter-crane"
+echo "[*] Copy the container ID of blazemeter-crane"
 
-echo "And enter the container id: "
+echo "[*] And enter the container id: "
 read ID
 
-sudo docker logs -f $ID >> crane.log
+sudo docker logs -f $ID | tee -a crane.log
 
